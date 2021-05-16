189b78d7f75625cd6d7d628c20fd4f64d43c96d0
Anibal Copitan <anibal@pprios.com>
2021-05-15 23:25:11 -0500

    [update] gotosection by URL (problem anchor move event native)

--- js/freelancer.js
+++ js/freelancer.js
@@ -6,19 +6,46 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        event.preventDefault();
-
-        var $linkCliked = $(this);
-        // MODIFIED FOR ANIBAL
+    var goToSectionWeb = function( IDselector ) {
+        var $section =  $( '#' + IDselector );
         var sumAllcomponentHeader = parseInt($('nav').css("padding-top"));
         sumAllcomponentHeader += parseInt($('nav').css("padding-bottom"));
         var sizeTotal =  $('nav').height() + sumAllcomponentHeader; // 60 nav
         
+        // goTo
         $('html, body').stop().animate({
-            scrollTop: $($linkCliked.attr('href')).offset().top - sizeTotal
+            scrollTop: $section.offset().top - sizeTotal
         }, 1500, 'easeInOutExpo');
+    };
+
+    // Access by Clik by links (anchors)
+    $('.page-scroll a').bind('click', function(event) {
+        // event.preventDefault();
+        var $linkCliked = $(this);
+        goToSectionWeb( $linkCliked.attr('href').split('#')[1] );
     });
+    
+    // Access by URL
+    var idHASH = document.location.toString().split('#')[1];
+    if (typeof(idHASH) == 'string') {
+        var theElement = document.querySelector('#' + idHASH);
+        if ( theElement != null) {
+            theElement.classList.remove('hide');
+
+            // navigate to section ONLY JS event
+            // document.querySelector('nav').remove();
+            // document.querySelector('.page-scroll a[href="#' + idHASH + '"]').click();
+
+            // **** set pposition
+            // var sizeNav = document.querySelector('nav').offsetHeight;
+            // var positionElement = theElement.offsetHeight;
+            // window.scrollTo(0, (theElement.offsetHeight + (sizeNav*2)))
+
+            // tricks for watch the section good (#link goto position by default)
+            // theElement.insertAdjacentHTML("afterbegin", "<br/><br/>");
+            goToSectionWeb(idHASH);
+        }
+    }
 });
 
 // Highlight the top nav as scrolling occurs
