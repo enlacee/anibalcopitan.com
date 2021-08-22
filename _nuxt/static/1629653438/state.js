51fe4838d86f45335445f79d7e6b75e0ce45f21d
Anibal Copitan <anibal@pprios.com>
2021-08-22 12:59:34 -0500

    [update] yarn buil & clean repo root

--- _nuxt/static/1629653438/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629653438",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d0fa264"}}}}(null,"\u002F"));
