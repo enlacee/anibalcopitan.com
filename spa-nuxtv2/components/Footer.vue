657ca041f4697075932c26f3b8ed66163624b605
Anibal Copitan <anibal@pprios.com>
2021-09-24 22:01:07 -0500

    [update] add click copy email

--- spa-nuxtv2/components/Footer.vue
+++ spa-nuxtv2/components/Footer.vue
@@ -15,10 +15,11 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-4"></div>
-                    <div class="col-md-3">
+                    <div class="col-md-6">
                         <address>
-                            <a href="mailto:contact@anibalcopitan.com">contact@anibalcopitan.com</a>
+                            <a id="input-text" href="mailto:contact@anibalcopitan.com">contact@anibalcopitan.com</a>
                         </address>
+                        <button id="button-click" class="btn btn-primary" @click="copyText()">Copy Email</button>
                     </div>
                 </div>
             </div>
@@ -27,6 +28,53 @@
 </template>
 <script>
 export default {
-    name: 'Footer'
+    name: 'Footer',
+
+    methods: {
+        copyText() {
+            function copyToClipboard(text) {
+                const dummy = document.createElement("input");
+                document.body.appendChild(dummy);
+                dummy.setAttribute('value', text);
+                dummy.select();
+                document.execCommand('copy');
+                document.body.removeChild(dummy);
+            }
+
+            /* Get the text field */
+            const copyText = document.getElementById("input-text").innerText;
+            copyToClipboard(copyText);
+
+            // effect text
+            const elButton = document.getElementById('button-click');
+            const primaryValue = elButton.innerText;
+            elButton.innerText = 'Copied!'
+            elButton.setAttribute('disabled', 'disabled');
+            setTimeout(function(){ 
+                elButton.removeAttribute('disabled');
+                elButton.innerText = primaryValue;
+            }, 700);
+
+        }
+    }
 }
 </script>
+<style scoped>
+    footer address {
+        display: inline-block;
+        margin-right: 1em;
+    }
+    button.btn {
+        padding: 0.3em 1em;
+        border: 1px solid white;
+        font-size: 0.7em;
+    }
+    button.btn:active {
+        background-color: black;
+        color: white;
+        border: 1px solid white;
+    }
+    button.btn[disabled] {
+        border: 1px solid gray;
+    }
+</style>
