fd379b997563ad165951b0fbb7c6f53d16539c92
Anibal Copitan <github@pprios.com>
2023-09-19 18:22:08 -0500

    yarn generate

---
+++ nuxt/static/1695165632/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1695165632",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"8d61a9ca"}}}}(null,"\u002F"));
