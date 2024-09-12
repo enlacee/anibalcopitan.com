854e3b980338f76afa829f335700160a58ef1f9a
Anibal Copitan <github@pprios.com>
2024-09-12 02:34:03 -0500

    [update] add subscription by mail to website

--- app-frontend/components/SectionHome.vue
+++ app-frontend/components/SectionHome.vue
@@ -10,6 +10,44 @@
                         </div>
                         <h2>{{ title }}</h2>
                     </div>
+<!-- 
+                    <div class="flex w-full flex-col items-center">
+                        <form class="group w-full rounded-wt bg-transparent" action="/create" method="post">
+                            <div class="flex flex-col"><div style="background-color:#F9FAFB;border:2px solid black" class="flex w-full flex-col items-center sm:flex-row overflow-hidden rounded-lg"><div class="flex w-full items-center" style="background-color:#F9FAFB"><div class="pl-2" style="color:black">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="h-6 w-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"></path></svg>
+                                </div><input type="email" autocomplete="email" required="" style="color:#111827" class="wt-button-font z-10 w-full border-none bg-transparent placeholder-shown:text-ellipsis text-lg focus:text-lg active:text-lg sm:text-lg" placeholder="Enter Your Email" name="email"></div><input type="submit" 
+                                style="background-color:black;color:#FFFCFC;" 
+                                class="cursor-pointer px-5 py-3 font-semibold w-full sm:w-auto text-lg focus:text-lg active:text-lg sm:text-lg rounded-lg rounded-none sm:rounded-lg-r" 
+                                value="Subscribe"></div>
+                            </div>
+                        </form>
+                    </div> -->
+                    <div class="form-container">
+                        <form class="subscription-form" action="https://anibalcopitan.us22.list-manage.com/subscribe/post?u=e9c62af441ddf5ac2469a4cf0&amp;id=f6003e68eb&amp;f_id=00fdd7e1f0" method="POST">
+                            <p class="form-description">
+                                ¡Descarga tu eBook de Linux y suscríbete a nuestras novedades!
+    </p>
+                            <div class="input-group">
+                            <div class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="icon-svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"></path>
+                                </svg>
+
+                                
+                            </div>
+                            <input type="email" autocomplete="email" required placeholder="Tu correo electrónico" name="EMAIL" class="email-input">
+                            
+                            <input type="submit" value="¡Descarga y suscríbete!" class="submit-btn">
+                            </div>
+
+                            <p class="form-note" style="font-size: 12px; padding-top: 0.5em;">No compartiremos tu correo. Puedes darte de baja en cualquier momento.</p>
+
+                        </form>
+                    </div>
+
+
+
+
                     <div id="list-conctact" class="intro-text">
                         <ul id="list-inline" class="list-inline">
                             <li>
@@ -85,5 +123,81 @@ export default {
     object-fit: cover;
     border: 2px solid #ddd; /* Borde sutil para destacar la imagen */
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Sombra ligera para un efecto profesional */
+}
+
+/* CSS FORMULARIO SUSCRIPCION */
+.form-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.subscription-form {
+  width: 100%;
+  max-width: 600px;
+}
+
+.input-group {
+  display: flex;
+
+  /* align-items: center; */
+  background-color: #F9FAFB;
+  border: 2px solid black;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.icon {
+  padding: 10px;
+  color: black;
+}
+
+.icon-svg {
+  width: 24px;
+  height: 24px;
+}
+
+.email-input {
+  flex: 1;
+  border: none;
+  padding: 10px;
+  font-size: 16px;
+  color: #111827;
+  background-color: transparent;
+  outline: none;
+}
+
+.email-input::placeholder {
+  color: #6B7280;
+}
+
+.submit-btn {
+  padding: 10px 20px;
+  background-color: black;
+  color: #FFF;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+
+.submit-btn:focus {
+  outline: none;
+}
+
+
+/* Media Query para móviles */
+@media (max-width: 600px) {
+  .input-group {
+    flex-direction: column; /* En móviles, apila los elementos verticalmente */
+  }
+
+  /* .subscription-form {
+        flex-wrap: wrap;
+        align-items: center;
+    } */
+.icon {
+    display: none;
+    }
 }
 </style>
