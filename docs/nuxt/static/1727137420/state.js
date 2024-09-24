c8653960e7f2fd4887a91168c8dff93689e046ea
Anibal Copitan <github@pprios.com>
2024-09-23 19:24:22 -0500

    [update] css intro website custom

---
+++ docs/nuxt/static/1727137420/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1727137420",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"1892853b"}}}}(null,"\u002F"));
