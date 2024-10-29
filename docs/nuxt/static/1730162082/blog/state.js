254bc100d8c93a6ea833bea9f4e3f045454dd31a
Anibal Copitan <github@pprios.com>
2024-10-28 19:37:06 -0500

    add: build html for production

---
+++ docs/nuxt/static/1730162082/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1730162082",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"5004bf22"}}}}(null));
